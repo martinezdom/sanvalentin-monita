ef8d36755d53e08756af2feac33b2f92fe4df498
Miguel Angel <tartanasdev@gmail.com>
2026-02-02 14:43:21 +0100

    Github pages fixed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import './assets/style.css'
 import App from './App.vue'
 
+// Handle 404.html redirect on GitHub Pages
+if (window.location.pathname.includes('?p=')) {
+  const pathname = decodeURIComponent(window.location.pathname.split('?p=')[1])
+  window.history.replaceState(null, null, pathname)
+}
+
 const app = createApp(App)
 const pinia = createPinia()
 
